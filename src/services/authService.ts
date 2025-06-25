3a9186752a7203576d172867778e57ebe827aa1f
Lazzzare <164561286+LazareChkhartishvili@users.noreply.github.com>
2025-06-25 11:39:02 +0400

    Refactor services to use Axios instance
    Replaced fetch with a shared Axios instance in authService and cartService for consistent API calls and improved error handling. Added axiosInstance.ts to centralize Axios configuration.

--- src/services/authService.ts
+++ src/services/authService.ts
@@ -1,19 +1,20 @@
+import { axiosInstance } from "./axiosInstance";
 import type { LoginPayload, LoginResponse } from "../types/auth";
+import axios from "axios";
 
 export const loginUser = async (
   payload: LoginPayload
 ): Promise<LoginResponse> => {
-  const response = await fetch("https://dummyjson.com/auth/login", {
-    method: "POST",
-    headers: { "Content-Type": "application/json" },
-    body: JSON.stringify(payload),
-  });
-
-  const data = await response.json();
-
-  if (!response.ok) {
-    throw new Error(data.message || "Login failed");
+  try {
+    const response = await axiosInstance.post<LoginResponse>(
+      "/auth/login",
+      payload
+    );
+    return response.data;
+  } catch (error: unknown) {
+    if (axios.isAxiosError(error)) {
+      throw new Error(error.response?.data?.message || "Login failed");
+    }
+    throw new Error("Login failed");
   }
-
-  return data;
 };
--- src/services/cartService.ts
+++ src/services/cartService.ts
@@ -1,33 +1,37 @@
+import { axiosInstance } from "./axiosInstance";
+import { AxiosError } from "axios";
+
 export const addToCart = async (
   userId: number,
   productId: number,
   quantity = 1,
   token: string
 ) => {
-  const response = await fetch("https://dummyjson.com/carts/add", {
-    method: "POST",
-    headers: {
-      "Content-Type": "application/json",
-      Authorization: `Bearer ${token}`,
-    },
-    body: JSON.stringify({
-      userId,
-      products: [
-        {
-          id: productId,
-          quantity,
+  try {
+    const response = await axiosInstance.post(
+      "/carts/add",
+      {
+        userId,
+        products: [
+          {
+            id: productId,
+            quantity,
+          },
+        ],
+      },
+      {
+        headers: {
+          Authorization: `Bearer ${token}`,
         },
-      ],
-    }),
-  });
+      }
+    );
 
-  const data = await response.json();
-
-  console.log(data);
-
-  if (!response.ok) {
-    throw new Error(data.message || "Failed to add to cart");
+    console.log(response.data);
+    return response.data;
+  } catch (error) {
+    const axiosError = error as AxiosError<{ message?: string }>;
+    throw new Error(
+      axiosError.response?.data?.message || "Failed to add to cart"
+    );
   }
-
-  return data;
 };
